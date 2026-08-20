1a5eb7eed48a29213e6cb105dee8ad31d4717f69
Ecrin Labs <alperenmesci@gmail.com>
2026-08-21 00:19:54 +0300

    Fix V7 duplicate TextView import

--- app/src/main/java/com/ecrinlabs/anyfile/MainActivity.kt
+++ app/src/main/java/com/ecrinlabs/anyfile/MainActivity.kt
@@ -34,7 +34,6 @@ import android.widget.LinearLayout
 import android.widget.ScrollView
 import android.widget.TextView
 import java.nio.charset.Charset
-import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.result.contract.ActivityResultContracts
